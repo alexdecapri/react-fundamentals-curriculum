034a5a5e7c7cec6e8c38c324370b3d4a362b96c2
alexdecapri <alexdecapri@gmail.com>
2016-07-12 13:20:10 -0600

    minor updates

--- app/components/Home.js
+++ app/components/Home.js
@@ -5,7 +5,21 @@ var styles = require('../styles');
 var Home = React.createClass({
   render: function() {
     return (
-      <div style={styles.homeContainer}>Home Test</div>
+      
+      <div style={styles.homeContainer} className="text-center">
+        <h1 style={styles.homeHeader}>
+          Enter a City and State
+        </h1>
+        <input
+          className="form-control"
+          placeholder="Anywhere, USA"
+          type="text" />
+        <button
+          className="btn btn-success"
+          type="submit">
+            Get Weather
+        </button>
+      </div>
 
     )
   }
--- app/styles/index.js
+++ app/styles/index.js
@@ -3,6 +3,11 @@ var styles = {
     backgroundImage: "url('app/images/pattern.svg')",
     width: '100%',
     height: '100%'
+  },
+  homeHeader: {
+    color: '#fff',
+    fontSize: 45,
+    fontweight: 100
   }
 };
 
